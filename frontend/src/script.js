80fb588fee3783f8c9a4d719533de53424dda987
Aakash Singh <aakash.fstack10@gmail.com>
2025-01-27 13:07:49 +0530

    Updated code in frontend/index.js

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -403,11 +403,14 @@ document
     };
 
     try {
-      const response = await fetch("http://localhost:3000/submit-form", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://backend.vercel.app/api/submit-form",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
